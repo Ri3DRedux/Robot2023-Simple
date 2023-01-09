63ce453b5418fb20ebbed9ec924733a886cf25bf
Chris Gerth <chrisgerth010592@gmail.com>
2023-01-09 09:50:11 -0600

    Second RObot WIP

---
+++ src/main/java/frc/robot/Intake.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+
+public class Intake {
+
+    PWMTalonSRX rollerMotor;
+    Solenoid extendSolenoid;
+    
+}
